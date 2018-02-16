672c092ec95d9f10cafe816b6473c8c7d183cb09
khan <khan@digitallyncs-MacBook-Air.local>
2018-02-16 16:13:26 +0530

    First class at NNRG

---
+++ numbersDemo.py
@@ -0,0 +1,72 @@
+# num = 10 
+# nu =4378894645604765465640563839789 
+# n = 10.23 
+# m = 36046707326476075478463046237407567405645356.43788946456047654656405639
+# print(type(num))
+# print(type(nu))
+# print(type(n))
+# print(type(m))
+
+# print(id(num))
+# print(id(nu))
+# print(id(n))
+# print(id(m))
+
+# import keyword 
+# print(keyword.kwlist)
+
+# import sys
+# print(sys.maxint)
+
+# x = 25
+# y = 7 
+
+# print(x/y) #3
+# print(x//y)#3
+
+# print(x+y) #32
+# print(x-y) #18
+# print(x*y) #175
+
+# print(x%y) #4
+# print(x**y)#6103515625
+
+# w = 25.0
+# z = 7
+
+# print(w/z) 
+# print(w//z)
+
+# a = 120
+# b = 140
+
+# print(a> b)
+# print(a< b)
+# print(a>=b)
+# print(a<=b)
+# print(a==b)
+# print(a!=b)
+
+
+# a = 120 
+# b = 40
+
+# print(a and b)
+# print(a or b)
+# print(not a)
+
+# a = 100 #00001010
+# b = 20  #00010100
+
+# print(a&b)
+# print(a|b)
+# print(a^b)
+# print(~a) 
+
+
+
+
+
+
+
+
