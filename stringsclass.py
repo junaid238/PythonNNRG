9d1325408af024cfdc1e2bbfbcc6230fc5e48106
pwa <pwa@PWAHYD-8009.processw.local>
2018-02-20 15:37:50 +0530

    Strings class one

---
+++ stringsclass.py
@@ -0,0 +1,58 @@
+# name = "python"
+# tech = 'data science'
+# para = "python is a most popular languafge now a days
+# 		python is a most popular languafge now a days
+# 			python is a most popular languafge now a days"
+# #comment
+# print(name)
+# print(tech)
+# newtech = "Pyt\nhon"
+# tabname = "data scie\t\tce"
+# print(newname)
+# print(newtech)
+# para = "python is a most popular languafge now a days\npython is a most popular languafge now a days\npython is a most popular languafge now a days"
+# print(para)
+
+# para = '''python is a most popular languafge now a days
+# python is a most popular languafge now a days
+# python is a most popular languafge now a days'''
+# print(para)
+
+# config = '''$ git push origin master
+# Counting objects: 3, done.
+# Delta compression using up to 4 threads.
+# Compressing objects: 100% (3/3), done.
+# Writing objects: 100% (3/3), 366 bytes | 366.00 KiB/s, done.
+# Total 3 (delta 1), reused 0 (delta 0)
+# remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+# To https://github.com/junaid238/PythonNNRG.git
+#    1d74bd7..413cce5  master -> master'''
+# print(config)
+
+# email = '''hello,
+# 		iam ***** , willing to take leave for 2 days.
+# 				thanks 
+# 	regards 
+# 	name '''
+# print(email)
+# print(type(email))
+
+# user = "nagesh"
+# pwd = 123
+# print("%s %d" %(user , pwd))
+
+# myChannel = r"https:\\www.youtube.com/khan/tutorials%search%definitions"
+# pathOfFile = r"Users\khan\python\newcode\tests"
+
+# print(myChannel)
+# print(pathOfFile)
+
+tech = "Python"
+
+print(tech[0])
+print(tech[3])
+print(tech[4])
+
+print(tech[-1])
+print(tech[-4])
+print(tech[-6])
